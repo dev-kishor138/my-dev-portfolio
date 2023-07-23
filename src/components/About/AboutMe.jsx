a2a5789c5812b591e57528858cff694fd3c94af5
Kishor Mahmud <dev.kishor138@gmail.com>
2023-07-23 21:32:47 +0600

    some changes

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -4,13 +4,7 @@ const AboutMe = () => {
   return (
     <div className="grid about-me">
       <p>
-        Hello, I am Ehaoteshamul Islam Kisor. I am a Full Stack Web Developer. I have a
-        strong passion about Web Development. When I completed the Web
-        Development Course With React. Here I gained a solid foundation in web
-        development principles, languages, and technologies, including HTML,
-        CSS, JavaScript, and frameworks like React. <br /> <br /> Then I did a job experience position at a junior developer in E-Learning and Earning LTD.
-        Here I get improve my skill in Full MERN Stack Development. Then I
-        create some projects. Now I am improving my skill with new technology.
+        My name is Ehaoteshamul Islam Kisor. I work as a full-stack web developer. I am really passionate about web development. when I finished the JavaScript Programming course. Here, I developed a strong foundation in web development principles, languages, and technologies, such as HTML, CSS, and JavaScript. Using Raw CSS, I built a few responsive websites. For a better user interface, I occasionally utilize Tailwind. Then I develop a project using the DOM and API. I post some code samples and a straightforward JavaScript project on GitHub. <br /> <br /> After that, I successfully completed an industrial training program in web development using MERN Stack at E-Learning and Earning LTD. Here, I can sharpen my Full MERN Stack Development skills.I developed a notion for using the MERN stack for whole website building. a solid understanding of the MERN Stack Technology as well as certain new technologies (such Axios, Firebase, Strapi, and JWT). These technologies have been used in some of my projects. Right now, I'm honing my technical skills.
       </p>
     </div>
   );
--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -4,24 +4,25 @@ const Info = () => {
   return (
     <div className="about-info grid">
       <div className="about-box">
-        <i className="bx bx-award about-icon"></i>
-        <h3 className="about-title">Experience</h3>
-        <span className="about-subtitle">1 Years</span>
+        <i className="bx bx-code-alt about-icon"></i>
+        <h3 className="about-title">Course</h3>
+        <span className="about-subtitle">Completed 2</span>
       </div>
+
       <div className="about-box">
         <i className="bx bx-briefcase about-icon"></i>
         <h3 className="about-title">Completed</h3>
         <span className="about-subtitle">30+ projects</span>
       </div>
-
-
-
-
       <div className="about-box">
-        <i className="bx bx-code-alt about-icon"></i>
-        <h3 className="about-title">Course</h3>
-        <span className="about-subtitle">Completed 3</span>
+        <i className="bx bx-award about-icon"></i>
+        <h3 className="about-title">Experience</h3>
+        <span className="about-subtitle">0 Years</span>
       </div>
+
+
+
+
     </div>
   );
 };
